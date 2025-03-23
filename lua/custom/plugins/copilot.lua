b15ec11d3309a554c4a717c3f15c4246d1dd826e
piking <patrick.kings@live.com>
2025-03-23 16:34:49 +0300

    add copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  'github/copilot.vim',
+  event = 'InsertEnter',
+  config = function()
+    -- Enable Copilot by default
+    vim.g.copilot_enabled = true
+    vim.g.copilot_no_tab_map = true -- Prevent default <Tab> mapping
+
+    -- Custom keymaps
+    vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
+    vim.api.nvim_set_keymap('i', '<C-K>', 'copilot#Previous()', { expr = true, silent = true })
+    vim.api.nvim_set_keymap('i', '<C-L>', 'copilot#Next()', { expr = true, silent = true })
+    vim.api.nvim_set_keymap('i', '<C-H>', 'copilot#Dismiss()', { expr = true, silent = true })
+  end,
+}
