a7742b7628f66aa21f2ac3f8a77e5c98bf3a3df0
piking <patrick.kings@live.com>
2025-03-26 09:18:44 +0300

    updates

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
+    -- markdown preview
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = 'cd app && pnpm i',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+
+    keys = {
+      { '<leader>mdp', ':MarkdownPreviewToggle<CR>', desc = 'Markdown Preview Toggle', silent = true },
+    },
+  },
+}
