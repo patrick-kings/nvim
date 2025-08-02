a4914a89050b88a135c6e537c90815e1775ed09a
kings <patrick.kings@live.com>
2025-08-02 22:03:15 +0300

    change readme

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -39,7 +39,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  -- require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
 
