89b55a1ad9162cbe3d48d4adf81b1f5889556958
kings <patrick.kings@live.com>
2025-09-17 23:30:34 +0300

    change outline

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -3,7 +3,9 @@ return {
   'hedyhli/outline.nvim',
   config = function()
     -- Example mapping to toggle outline
-    vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+    vim.keymap.set('n', '<leader>co', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+    vim.keymap.set('n', '<leader>o', '<cmd>OutlineFocus<CR>', { desc = 'Focus Outline' })
 
     require('outline').setup {
       -- Your setup opts here (leave empty to use defaults)
